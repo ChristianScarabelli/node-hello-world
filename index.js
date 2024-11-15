3be73dc3b90cb80388f83c75abc4fc8c5663e1c6
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-15 09:43:43 +0100

    ADD: comment

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ console.log('Hello Boolean')
 console.log(process.argv)
 
 const word = process.argv[2]
+// const word = process.argv.slice(2) // recuperare in array i valori dall'indice 2 in poi (valori passati da noi)
 console.log(`Hello ${word}`)
